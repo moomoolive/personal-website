03e112e32bb112c023f9ba9ca1244bffb1efaac0
Mostafa Elbannan <mostafaelbannan1@gmail.com>
2021-04-26 14:10:21 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const app = window.document.createElement('div')
+app.id = "app"
+app.innerHTML = "hello from webpack"
+
+window.document.body.appendChild(app)
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new htmlWebpackPlugin({
+            title: "Mostafa's Website"
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        hot: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            }
+        ]
+    }
+}
